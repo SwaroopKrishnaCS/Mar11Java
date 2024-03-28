8a0781dd0546bddfffeec1b860448f2c6358b0bc
SwaroopKrishnaCS <panicker.swaroop@gmail.com>
2024-03-27 21:28:42 -0400

    First Class

---
+++ src/firstDayJava/MobilePhone.java
@@ -0,0 +1,33 @@
+package firstDayJava;
+
+public class MobilePhone {
+	
+	//DataType
+	
+	//DataType variableName = value
+	String colour = "White";
+	
+	//byte, int, long, float, double
+	int memory = 8;
+	
+	double screenSize = 6.5;
+	
+	String material = "Metal";
+	
+	float cameraConfiguration = 1.3f;
+	
+	boolean isFaceRecogAvailable = true;
+	
+	double weight = 160.5;
+	
+	String brandName = "IPhone";
+	
+	
+	//Method or Function
+	void makeCalls() {
+		//print a line
+		System.out.println("Making a call");
+		
+	}
+	
+}
