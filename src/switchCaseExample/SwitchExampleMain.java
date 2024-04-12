98aa3697573dae7a90359e5e99869b0b19c6e6ac
Manvir <manvir.singh.parmar@gmail.com>
2024-04-11 21:59:46 -0400

    Switch example

---
+++ src/switchCaseExample/SwitchExampleMain.java
@@ -0,0 +1,14 @@
+package switchCaseExample;
+
+public class SwitchExampleMain {
+
+	public static void main(String[] args) {
+		
+		
+		SwitchExample switchExample=new SwitchExample();
+		//switchExample.exampleOfSwitchCase();
+		switchExample.daysOfWeek();
+
+	}
+
+}
