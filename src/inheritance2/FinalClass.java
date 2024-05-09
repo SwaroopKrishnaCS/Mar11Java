f3735a4e126221ac5386119ef3af20bd64bb286c
ketakipillay <ketakipillay@gmail.com>
2024-05-08 20:56:44 -0400

    Final Keyword

---
+++ src/inheritance2/FinalClass.java
@@ -0,0 +1,27 @@
+package inheritance2;
+
+public final class FinalClass {
+
+	public String a;
+	
+	public FinalClass(String a) {
+		super();
+		this.a = a;
+	}
+
+	public FinalClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void method1() {
+		
+	}
+	
+	public void method2() {
+		
+	}
+	
+	public void method3() {
+		
+	}
+}
